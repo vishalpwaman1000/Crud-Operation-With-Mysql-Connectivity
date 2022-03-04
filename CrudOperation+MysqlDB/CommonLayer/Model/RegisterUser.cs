948c323fb2c765ae4f8f2c91a003294750a9c35c
vishalpwaman123 <1000thebeast1000@gmail.com>
2022-03-04 10:47:43 +0530

    Added

---
+++ CrudOperation+MysqlDB/CommonLayer/Model/RegisterUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudOperation_MysqlDB.CommonLayer.Model
+{
+    public class RegisterUserRequest
+    {
+        [Required(ErrorMessage ="UserName Is Required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Password Is Required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Confirm Password Is Required")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterUserResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
